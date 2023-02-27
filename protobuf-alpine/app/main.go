7f2b2d36ef4720197012f5157931c40aa047e592
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-27 13:32:34 +0900

    implemented grpc server

--- protobuf-alpine/app/main.go
+++ protobuf-alpine/app/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"net"
+
+	"app/protobuf"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type PingServer struct {
+	protobuf.UnimplementedPingServiceServer
+}
+
+func (p *PingServer) Ping(context.Context, *protobuf.PingRequest) (*protobuf.PingResponse, error) {
+	return &protobuf.PingResponse{Message: "Hello, ICTSC2022 Contestant!"}, nil
+}
+
+func main() {
+	ping := &PingServer{}
+
+	server := grpc.NewServer()
+	protobuf.RegisterPingServiceServer(server, ping)
+
+	reflection.Register(server)
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Panic(server.Serve(listener))
+}
